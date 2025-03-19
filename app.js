0815012f991fb6416557fea9ba6696ce77cc0bd1
GPucheu <germanpucheu@gmail.com>
2025-03-18 23:22:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ullistaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo(){ 
+	listaAmigos.push(amigo,value);
+	alert("Digite un nombre correcto");
+	lista.innerHTML += '<li>${amigo,value}</li>';
+	
+ };
+ function sortearAmigo(){ 
+	const random = Math.fñoor(Math.random() * listaAmigos.lenght);
+	const amigoSecreto = listaAmigos[random];
+	ulResultado.innerHTML = '<li>el amigo secreto es:${amigoSecreto}</li>';
+	
+ };
